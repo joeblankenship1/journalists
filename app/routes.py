220a7d8656d0d00077112de805e22a442c70ee30
Joe Blankenship <joe.blankenship1@gmail.com>
2018-07-13 16:05:06 -0400

    added templates to app

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,14 @@
+from flask import render_template, flash
 from app import app
+from app.forms import QueryForm
 
 
 @app.route('/')
 @app.route('/index')
-
 def index():
-    return "Home - Journalist App"
+    form = QueryForm()
+    if form.validate_on_submit():
+        flash('Query required'.format(
+            form.query.data
+        ))
+    return render_template('index.html', title='Home', form=form)
