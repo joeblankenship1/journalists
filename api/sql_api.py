6687b9f2a4d5c052b55c12f371d5bd95b43329f4
Joe Blankenship <joe.blankenship1@gmail.com>
2018-07-16 22:59:11 -0400

    updated import calls, added CORS support

--- api/sql_api.py
+++ api/sql_api.py
@@ -1,13 +1,17 @@
 import flask
-import flask.ext.sqlalchemy
-import flask.ext.restless
+import flask_sqlalchemy
+import flask_restless
+from flask_cors import CORS
 
 
 app = flask.Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/app.db'
+app.config['CORS_ALLOW_HEADERS'] = "Content-Type"
+app.config['CORS_RESOURCES'] = {r"/api/*": {"origins": "*"}}
 
-db = flask.ext.sqlalchemy.SQLAlchemy(app)
+db = flask_sqlalchemy.SQLAlchemy(app)
+cors = CORS(app)
 
 
 class Journalist(db.Model):
@@ -20,7 +24,7 @@ class Journalist(db.Model):
 
 db.create_all()
 
-manager = flask.ext.restless.APIManager(app, flask_sqlalchemy_db=db)
+manager = flask_restless.APIManager(app, flask_sqlalchemy_db=db)
 manager.create_api(Journalist,
                    methods=['GET'],
                    max_results_per_page=1000)
